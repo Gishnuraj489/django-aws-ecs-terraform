a29e1f09376114177ccb238108bcce2fb4c64c08
Gishnuraj S K <143984810+Gishnuraj489@users.noreply.github.com>
2025-05-31 16:03:52 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,2 @@
 # provider.tf
 
-# Specify the provider and access details
-provider "aws" {
-  profile = "default"
-  region  = terraform.workspace
-  default_tags {
-    tags = {
-      Environment = terraform.workspace
-    }
-  }
-}
