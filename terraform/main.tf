47c9d8a11a66698ce80fecfb901cf2b270483ee4
Gishnuraj S K <143984810+Gishnuraj489@users.noreply.github.com>
2025-05-31 15:59:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,30 @@
-module "ecs_app" {
-  source                       = "./modules/ecs"
-  ec2_task_execution_role_name = "EcsTaskExecutionRoleName"
-  ecs_auto_scale_role_name     = "EcsAutoScaleRoleName"
-  app_image                    = "167365792572.dkr.ecr.us-west-1.amazonaws.com/django-app:production"
-  app_port                     = 8000
-  app_count                    = 1
-  health_check_path            = "/"
-  fargate_cpu                  = "1024"
-  fargate_memory               = "2048"
-  aws_region                   = terraform.workspace
-  az_count                     = "2"
-  subnets                      = module.network.public_subnet_ids
-  sg_ecs_tasks                 = [module.security.ecs_tasks_security_group_id]
-  vpc_id                       = module.network.vpc_id
-  lb_security_groups           = [module.security.alb_security_group_id]
+provider "aws" {
+  region = var.region
 }
 
-module "network" {
-  source   = "./modules/network"
-  az_count = "2"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-module "security" {
-  source   = "./modules/security"
-  app_port = 80
-  vpc_id   = module.network.vpc_id
+resource "aws_subnet" "subnet" {
+  count                   = 2
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 }
 
-module "logs" {
-  source            = "./modules/logs"
-  log_group_name    = "/ecs/ecs-app"
-  log_stream_name   = "ecs-log-stream"
-  retention_in_days = 30
-}
-
-module "remote_backend" {
-  source              = "./modules/backend"
-  bucket_name         = "terraform-state-backend"
-  dynamodb_table_name = "terraform-state-lock-table"
-}
-
-module "rds" {
-  source     = "./modules/rds"
-  depends_on = [module.network.vpc_id]
-  subnet_ids = module.network.private_subnet_ids
-  vpc_id     = module.network.vpc_id
-  ecs_tasks_security_group_id = module.security.ecs_tasks_security_group_id
-}
-
-
-module "s3" {
-  source = "./modules/s3_img"
-  bucket_name = "bucket-image-codewithmuh-454"
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
